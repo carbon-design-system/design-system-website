59add71baef71cde3d7d6ed2461e0fb80e4a59a6
thisisbrianhan <bthan@us.ibm.com>
2016-03-01 14:09:54 -0600

    Refactor gulpfile.js
    Convert tabs to spaces
    
    Update to newest bluemix-components 4.6.3-alpha
    
    Fix indents
    
    Update glob pattern for gulp.watch
    
    Change browserSync and gulp.watch for styles
    
    * Set browserSync to open: false - it's annoying to see it open automatically
    * gulp.watch for styles does not need to watch for 'change' event
    
    Update use of browserSync and gutil for setting env
    
    Remove unused node_modules
    
    npm run build does: gulp clean then gulp build
    
    Rearrange tasks
    
    Update scripts for install
    
    Remove unused gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,7 @@
 // MODULES                   //
 ///////////////////////////////
 var assemble = require('fabricator-assemble');
-var browserify = require('browserify');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 var concat = require('gulp-concat');
 var csso = require('gulp-csso');
 var del = require('del');
@@ -16,12 +15,7 @@ var imagemin = require('gulp-imagemin');
 var prefix = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
 var markdown = require('gulp-markdown');
-var reload = browserSync.reload;
-var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
-var source = require('vinyl-source-stream');
-var streamqueue = require('streamqueue');
-var streamify = require('gulp-streamify');
 var uglify = require('gulp-uglify');
 
 
@@ -29,38 +23,33 @@ var uglify = require('gulp-uglify');
 // CONFIGURATION             //
 ///////////////////////////////
 
+// Set env with --env=$env flag
+// like this: gulp serve --env=dev
+// Source: http://ypereirareis.github.io/blog/2015/10/13/gulp-gulpfile-environment-variable/
+var env = gutil.env.env || undefined;
+
 var config = {
-    dev: gutil.env.dev,
-    src: {
-        scripts: {
-            main: [
-                'src/assets/scripts/main.js'
-            ],
-            vendor: [
-                'src/assets/scripts/prism.js'
-            ]
-        },
-		styles: {
-			main: 'src/assets/styles/main.scss'
-		},
-		views: 'src/views/*.html',
-        bluemix: {
-            compMarkdown: 'bower_components/bluemix-components/components/**/**/*.md',
-            baseMarkdown: 'bower_components/bluemix-components/base-elements/**/**/*.md'
-        }
+  src: {
+    scripts: {
+      main: [
+        'src/assets/scripts/main.js'
+      ],
+      vendor: [
+        'src/assets/scripts/prism.js'
+      ]
     },
-    dest: 'dist'
+    styles: {
+      main: 'src/assets/styles/main.scss',
+      partials: 'src/assets/styles/partials/**/*.scss'
+    },
+    views: 'src/views/*.html',
+    bluemix: {
+      compMarkdown: 'bower_components/bluemix-components/components/**/**/*.md',
+      baseMarkdown: 'bower_components/bluemix-components/base-elements/**/**/*.md'
+    }
+  },
+  dest: 'dist'
 };
-
-
-
-///////////////////////////////
-// CLEAN                     //
-///////////////////////////////
-
-gulp.task('clean', function (cb) {
-    del(['**'], { ignore: 'demo/**', cwd: config.dest }, cb);
-});
 
 
 ///////////////////////////////
@@ -80,15 +69,17 @@ gulp.task('copy', function() {
 ///////////////////////////////
 
 gulp.task('styles', function () {
-	return gulp.src(config.src.styles.main)
-		.pipe(sass({
-			errLogToConsole: true
-		}))
-		.pipe(prefix('last 1 version'))
-		.pipe(gulpif(!config.dev, csso()))
-		.pipe(rename('main.css'))
-		.pipe(gulp.dest(config.dest + '/assets/styles'))
-		.pipe(gulpif(config.dev, reload({stream:true})));
+  return gulp.src(config.src.styles.main)
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(prefix({
+      browsers: ['> 1%', 'last 2 versions']
+    }))
+    .pipe(gulpif(env !== 'dev', csso()))
+    .pipe(rename('main.css'))
+    .pipe(gulp.dest(config.dest + '/assets/styles'))
+    .pipe(gulpif(env === 'dev', browserSync.stream()));
 });
 
 
@@ -97,10 +88,10 @@ gulp.task('styles', function () {
 ///////////////////////////////
 
 gulp.task('scripts', function () {
-    return gulp.src(config.src.scripts.main)
-        .pipe(concat('main.js'))
-        .pipe(gulpif(!config.dev, uglify()))
-        .pipe(gulp.dest(config.dest + '/assets/scripts'));
+  return gulp.src(config.src.scripts.main)
+    .pipe(concat('main.js'))
+    .pipe(gulpif(env !== 'dev', uglify()))
+    .pipe(gulp.dest(config.dest + '/assets/scripts'));
 });
 
 
@@ -109,14 +100,14 @@ gulp.task('scripts', function () {
 ///////////////////////////////
 
 gulp.task('markdown:components', function() {
-    return gulp.src(config.src.bluemix.compMarkdown)
+  return gulp.src(config.src.bluemix.compMarkdown)
     .pipe(rename({dirname: ''}))
     .pipe(markdown())
     .pipe(gulp.dest('src/materials/components/'));
 });
 
 gulp.task('markdown:base-elements', function() {
-    return gulp.src(config.src.bluemix.baseMarkdown)
+  return gulp.src(config.src.bluemix.baseMarkdown)
     .pipe(rename({dirname: ''}))
     .pipe(markdown())
     .pipe(gulp.dest('src/materials/base-elements/'));
@@ -130,77 +121,70 @@ gulp.task('markdown', ['markdown:components', 'markdown:base-elements']);
 ///////////////////////////////
 
 gulp.task('assemble', function(done) {
-    assemble({
-        helpers: {
-            markdown: function (str, opts) {
-                if (typeof str === 'object') {
-                    opts = str;
-                    str = null;
-                }
-                str = str || opts.fn(this);
-                return require('markdown-it')()
-                    .use(require('markdown-it-headinganchor'), {
-                        linkify: true,
-                        anchorClass: '',
-                        slugify: function(str) {
-                            return require('string')(str).slugify().s;
-                        }
-                    })
-                    .render(str);
-            },
-            decode: function (val) {
-                return decodeURIComponent(val);
-            },
-            raw: function (options) {
-                return options.fn();
-            }
+  assemble({
+    helpers: {
+      markdown: function (str, opts) {
+        if (typeof str === 'object') {
+          opts = str;
+          str = null;
         }
-    });
-    done();
+        str = str || opts.fn(this);
+        return require('markdown-it')()
+          .use(require('markdown-it-headinganchor'), {
+              linkify: true,
+              anchorClass: '',
+              slugify: function(str) {
+                  return require('string')(str).slugify().s;
+              }
+          })
+          .render(str);
+      },
+      decode: function (val) {
+        return decodeURIComponent(val);
+      },
+      raw: function (options) {
+        return options.fn();
+      }
+    }
+  });
+  done();
 });
 
+
+///////////////////////////////
+// CLEAN                     //
+///////////////////////////////
+
+gulp.task('clean', function (cb) {
+  del(['**'], { ignore: 'demo/**', cwd: config.dest }, cb);
+});
+
+
+///////////////////////////////
+// BUILD                     //
+///////////////////////////////
+
+gulp.task('build', ['copy', 'markdown', 'styles', 'scripts', 'assemble']);
 
 ///////////////////////////////
 // SERVE                     //
 ///////////////////////////////
 
-gulp.task('serve', ['default'], function () {
+gulp.task('serve', function () {
 
-    var reload = browserSync.reload;
+  var reload = browserSync.reload;
 
-    browserSync({
-        server: {
-            baseDir: config.dest
-        },
-        notify: false,
-        logPrefix: 'BLUEMIX DESIGN SYSTEM'
-    });
+  browserSync.init({
+    server: {
+      baseDir: config.dest
+    },
+    notify: true,
+    open: false,
+    logPrefix: 'Bluemix Design System'
+  });
 
-    gulp.watch('src/materials/**/**/**/*.{html,md,json,yml}', ['assemble']).on('change', reload);
-    gulp.watch('src/assets/styles/**/*.scss', ['styles']).on('change', reload);
-    gulp.watch('src/assets/scripts/**/*.js', ['scripts']).on('change', reload);
-    gulp.watch(config.src.images, ['images']).on('change', reload);
-});
-
-
-///////////////////////////////
-// DEFAULT BUILT TAS         //
-///////////////////////////////
-
-gulp.task('default', ['clean'], function () {
-
-    var tasks = [
-        'copy',
-        'markdown',
-        'styles',
-        'scripts',
-        'assemble'
-    ];
-
-    runSequence(tasks, function () {
-        if (config.dev) {
-            gulp.start('serve');
-        }
-    });
-
+  gulp.watch('src/materials/**/*.{html,md,json,yml}', ['assemble']).on('change', reload);
+  gulp.watch('src/**/*.scss', ['styles']);
+  gulp.watch('src/assets/scripts/**/*.js', ['scripts']).on('change', reload);
+  gulp.watch(config.src.images, ['images']).on('change', reload);
 });
