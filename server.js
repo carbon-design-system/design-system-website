886bf5c5db6fb24cb8950fe99fde92c34f02245d
Mari Johannessen <mari.l.johannessen@gmail.com>
2016-03-14 17:06:12 -0500

    Added routes and pages, fixed up some styling issues with the menu
    Fixed up some node modules
    
    Added variable for snippet bg
    
    Fixed a few things after PR comments
    
    Fixed indentation
    
    Added route for templates

--- server.js
+++ server.js
@@ -8,8 +8,25 @@ const port = process.env.PORT || 8080;
 app.set('views', path.resolve(__dirname, 'dist'));
 app.use(express.static('dist'));
 
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/base-elements', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/base-elements.html'));
+});
+
+app.get('/components', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/components.html'));
+});
+
+app.get('/guidelines', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/guidelines.html'));
+});
+
+app.get('/templates', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/templates.html'));
 });
 
 app.listen(port, () => {
