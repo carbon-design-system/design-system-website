f9d8417f4d0eb36beba8ff3e764991912fc1b20a
Mari Johannessen <mari.l.johannessen@gmail.com>
2016-03-25 10:58:41 -0500

    Slide in/out sidebar
    Added fab button
    
    Finished up menu slide
    
    Removed fab button
    
    Fixed up the sidebar nav to be responsive and floating on smaller screens
    
    Addressed PR comments

--- src/assets/scripts/site/menu.js
+++ src/assets/scripts/site/menu.js
@@ -1,6 +1,28 @@
 'use strict';
 
 const itemHeadings = document.querySelectorAll('.item__heading');
+const menu = document.querySelector('.menu');
+const container = document.querySelector('.container');
+const toggleButton = document.querySelector('.sidebar-toggle');
+const itemLinks = document.querySelectorAll('.item__link');
+
+export const toggleMenu = () => {
+  toggleButton.addEventListener('click', function(e) {
+    toggleButton.classList.toggle('active');
+    menu.classList.toggle('is-hidden');
+    container.classList.toggle('is-expanded');
+  });
+}
+
+export const activeMenuLink = () => {
+  Array.prototype.forEach.call(itemLinks, function(element) {
+    let itemPath = element.pathname.split('/')[2];
+    let windowPath = window.location.href.split('/')[4];
+    if (itemPath == windowPath) {
+      element.parentElement.classList.add('active');
+    }
+  })
+}
 
 export const openItemHeadings = (nodeList = itemHeadings) => {
   Array.prototype.forEach.call(nodeList, function(element) {
