9def4cf2ebb60c61aad519a423d118ba4070b8c3
BRIAN T. HAN <bthan@us.ibm.com>
2016-05-30 13:09:04 -0500

    Refactor main.js (#306)
    * Import all bluemix-components js from index
    
    * Named imports for functions from ./site/menu
    
    * Remove use of Array.protoype.forEach

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -18,27 +18,18 @@ import './vendor/prism';
 // -------------
 // Import all production JS (ES2015) files from bluemix-components
 
-// Base Elements
-import FileUploader from '../../../bower_components/bluemix-components/consumables/js/es2015/file-uploader';
-
-// Components
-import Spinner from '../../../bower_components/bluemix-components/consumables/js/es2015/loading';
-import Modal from '../../../bower_components/bluemix-components/consumables/js/es2015/modals';
-import OverflowMenu from '../../../bower_components/bluemix-components/consumables/js/es2015/overflow-menu';
-import Tab from '../../../bower_components/bluemix-components/consumables/js/es2015/tabs';
-import Table from '../../../bower_components/bluemix-components/consumables/js/es2015/table';
-import Dropdown from '../../../bower_components/bluemix-components/consumables/js/es2015/dropdown';
-import ContentSwitcher from '../../../bower_components/bluemix-components/consumables/js/es2015/content-switcher';
-import { NumberInput } from '../../../bower_components/bluemix-components/consumables/js/es2015/index';
+import {
+  // Base Elements
+  FileUploader, NumberInput, Table,
+  // Components
+  ContentSwitcher, Dropdown, Loading, Modal, OverflowMenu, Tab,
+} from '../../../bower_components/bluemix-components/consumables/js/es2015/index';
 
 ////////////////////////////////////////////////////////////////////////
 // Site
 // -------------
 // Import specific scripts for controlling website behavior
-import { openItemHeadings } from './site/menu';
-import { toggleMenu } from './site/menu';
-import { activeMenuLink } from './site/menu';
-import { autoCloseMenu } from './site/menu';
+import { openItemHeadings, toggleMenu, activeMenuLink, autoCloseMenu } from './site/menu';
 import { tabsNavigation } from './site/tabs';
 import { codeSnippets } from './site/snippets';
 import { themeSwitcher } from './site/theme-switcher';
@@ -50,18 +41,12 @@ import { expandType } from './site/expand-type';
 
 document.addEventListener('DOMContentLoaded', function(e) {
 
-  [... document.querySelectorAll('[data-file-input]')].forEach((element) => new FileUploader(element));
-  [... document.querySelectorAll('[data-spinner]')].forEach((element) => {
-    const spinner = new Spinner(element);
-    setInterval(() => spinner.toggle(), 3000);
+  [... document.querySelectorAll('[data-loading]')].forEach((element) => {
+    // Toggles on and off animations for Loading as a demo but may be misleading for users
+    setInterval(() => Loading.components.get(element).toggle(), 3000);
   });
-  [... document.querySelectorAll('[data-modal-target]')].forEach((element) => Modal.init(element));
-  [... document.querySelectorAll('[data-overflow-menu]')].forEach((element) => new OverflowMenu(element));
-  [... document.querySelectorAll('[data-tabs]')].forEach((element) => new Tab(element));
-  [... document.querySelectorAll('[data-table]')].forEach((element) => new Table(element));
-  [... document.querySelectorAll('[data-dropdown]')].forEach((element) => new Dropdown(element));
-  [... document.querySelectorAll('[data-content-switcher]')].forEach((element) => new ContentSwitcher(element));
-  Array.prototype.forEach.call(document.querySelectorAll('[data-tabs] .item__link'), function (element) {
+
+  [... document.querySelectorAll('[data-tabs] .item__link')].forEach((element) => {
     // Don't follow links in tab
     element.addEventListener('click', function (event) {
       event.preventDefault();
