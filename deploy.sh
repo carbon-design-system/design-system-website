de3b852778cacd1fc0516987b5c25bac622bb89b
Christopher A. Dhanaraj <chrisdhanaraj@us.ibm.com>
2016-11-28 13:35:48 -0800

    forgot a dash (#730)

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-curl -L "https://cli.run.pivotal.io/stable?release=linux64binary&source=github" | tar -zx
+curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
 ./cf api https://api.ng.bluemix.net
 ./cf login -u chrisdhanaraj@us.ibm.com -p $password -o chrisdhanaraj_org -s Bluemix Design System
 ENV=external
