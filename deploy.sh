5ed139cfe921dd59c4a76b96cff46feb4bc3ad38
Christopher A. Dhanaraj <chrisdhanaraj@us.ibm.com>
2016-11-28 15:13:04 -0800

    Use the production registry (#737)
    * use the production registry
    
    * move system env to npm run build

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
 ./cf api https://api.ng.bluemix.net
 ./cf login -u chrisdhanaraj@us.ibm.com -p $password -o chrisdhanaraj_org -s "Bluemix Design System"
-ENV=external
-npm run build
+ENV=external npm run build
 ./cf push
