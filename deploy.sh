7776a33716debadf922568d4f9a17eff773480fa
Christopher A. Dhanaraj <chrisdhanaraj@us.ibm.com>
2016-11-28 14:00:41 -0800

    Install in travis instead (#733)
    * install in travis instead
    
    * remove install in deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,5 @@ curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github"
 ./cf api https://api.ng.bluemix.net
 ./cf login -u chrisdhanaraj@us.ibm.com -p $password -o chrisdhanaraj_org -s "Bluemix Design System"
 ENV=external
-npm install
 npm run build
 ./cf push
