127645151ee7940be55188cc8d39ae9e14eeb88e
Christopher A. Dhanaraj <chrisdhanaraj@us.ibm.com>
2016-11-28 17:38:00 -0800

    Bgd (#742)
    * update deploy script
    
    * bluegreen deploys
    
    * add force in

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
+./cf install-plugin blue-green-deploy -r CF-Community -f
 ./cf api https://api.ng.bluemix.net
 ./cf login -u chrisdhanaraj@us.ibm.com -p $password -o chrisdhanaraj_org -s "Bluemix Design System"
 ENV=external npm run build
-./cf push
+./cf blue-green-deploy design-system
 ./cf api https://api.stage1.ng.bluemix.net
 ./cf login -u chrisdhanaraj@us.ibm.com -p $password -o "Bluemix Dev" -s shared
 ENV=internal npm run build
-./cf push
+./cf blue-green-deploy design-system
