62cff3e08d08651686491ed85869e27629490916
Christopher A. Dhanaraj <chrisdhanaraj@us.ibm.com>
2016-11-28 17:25:05 -0800

    update deploy script (#741)

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,7 @@ curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github"
 ./cf login -u chrisdhanaraj@us.ibm.com -p $password -o chrisdhanaraj_org -s "Bluemix Design System"
 ENV=external npm run build
 ./cf push
+./cf api https://api.stage1.ng.bluemix.net
+./cf login -u chrisdhanaraj@us.ibm.com -p $password -o "Bluemix Dev" -s shared
+ENV=internal npm run build
+./cf push
