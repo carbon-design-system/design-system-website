e96ca1440fb807e733318143030f821aecd18c8d
MARI L. JOHANNESSEN <mjohannessen@us.ibm.com>
2016-12-15 11:40:32 -0600

    Iconography page fix for IE11 and Firefox (#782)
    * started fixing icons page
    
    * fixed iconography page on ie11

--- src/assets/scripts/site/icon-search.js
+++ src/assets/scripts/site/icon-search.js
@@ -1,16 +1,13 @@
 const iconSearch = document.querySelector('.icon-search');
-const iconList = document.querySelector('.icon-container');
 const iconItems = [... document.querySelectorAll('.icon')];
 
 const queryIcons = () => {
-  if (iconList) {
-    iconList.innerHTML = '';
-  }
-
   iconItems.forEach((icon) => {
-    const queryTest = icon.getAttribute('data-tags').indexOf(iconSearch.value);
+    const currentIcon = icon;
+    currentIcon.style.display = 'none';
+    const queryTest = currentIcon.getAttribute('data-tags').indexOf(iconSearch.value);
     if (queryTest > -1) {
-      iconList.appendChild(icon);
+      currentIcon.style.display = 'block';
     }
   });
 };
