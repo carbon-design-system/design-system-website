5cba6d5492eb2a3afb8c91db72fd799e31eafe89
Ian Fleming <ianfleming@us.ibm.com>
2017-01-19 14:08:18 -0600

    Add Color Page (#877)
    * started setting up environment
    
    * added eslint and tested out routes:
    
    * added fonts back in
    
    * WIP add ColorCard component and color page
    
    * Finish ColorCard component
    
    * Add Color package to check contrast
    
    * Add settimeout to reset copy button text

--- src/pages/style/Colors.js
+++ src/pages/style/Colors.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import ColorCard from '../../components/ColorCard/ColorCard';
+import ColorList from '../../data/colors.json';
 
 class Colors extends React.Component {
+  renderColorCards = ColorItems =>
+    Object.keys(ColorItems).map((ColorItem) => {
+      const ColorItemObj = ColorItems[ColorItem];
+      return (
+        <ColorCard
+          name={ColorItemObj.name}
+          hex={ColorItemObj.hex}
+          scss={ColorItemObj.scss}
+        />
+      );
+    });
 
   render() {
+    const ColorCards = this.renderColorCards(ColorList['light-ui-colors']);
+    const AccentColorCards = this.renderColorCards(ColorList['light-ui-accent-colors']);
+
     return (
-      <h1>Colors page here</h1>
+      <div>
+        <p>Color brings a design to life. Color is versatile;
+          it&apos;s used to express emotion and tone, as well as place
+          emphasis and create associations. Color should always
+          be used in meaningful and intentional ways that create patterns and visual cues.</p>
+        <ul className="wrapped-list">
+          {ColorCards}
+          {AccentColorCards}
+        </ul>
+      </div>
     );
   }
 }
