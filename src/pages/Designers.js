c19216863443881dc36b0db5ae52e9b14293b735
MARI L. JOHANNESSEN <mjohannessen@us.ibm.com>
2017-03-02 14:35:26 -0600

    Fixed gitignore (#911)
    * started setting up environment
    
    * added eslint and tested out routes:
    
    * fixed merge conflict
    
    * removed config from gitignore
    
    * removed config
    
    * added config

---
+++ src/pages/Designers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Designers extends React.Component {
+
+  render() {
+    return (
+      <h1>Designers</h1>
+    );
+  }
+}
+
+export default Designers;
---
+++ src/pages/Developers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Developers extends React.Component {
+
+  render() {
+    return (
+      <h1>Developers</h1>
+    );
+  }
+}
+
+export default Developers;
