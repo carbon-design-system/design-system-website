b29321b235b41afc1932446b75491e9237aeb69e
BRIAN T. HAN <bthan@us.ibm.com>
2017-04-18 13:59:05 -0500

    Update icon page (#1030)
    * fix(package.json): use carbon-icons@5.1.2
    
    Older versions of carbon-icons only allows node 6.0.0 to be used. This
    made it impossible to install.
    
    * fix(Iconography.js): use latest carbon-icons and add
    tempModifyIconName()
    
    Added temp method to remove 'icon--' from Icon.name to correctly resolve
    paths to assets/svg filenames
    
    * fix(Iconography.js): get package.json from carbon-components
    
    * refacotr(Iconography.js): simplify import of carbon-icons

--- src/pages/style/Iconography.js
+++ src/pages/style/Iconography.js
@@ -1,18 +1,16 @@
 import React, { PropTypes } from 'react';
-import IconList from
- '../../../node_modules/@console/bluemix-icons/bluemix-icons.json';
+import IconList from 'carbon-icons';
 import Tab from '@console/bluemix-components-react/dist/components/Tab';
 import Search from '@console/bluemix-components-react/dist/components/Search';
 import PageTabs from '../../internal/PageTabs';
 import IconCard from '../../internal/IconCard';
 import MarkdownPage from '../../internal/MarkdownPage';
-import Packages from '@console/bluemix-components-react/package.json';
+import Packages from 'carbon-components/package.json';
 
 class Iconography extends React.Component {
   static propTypes = {
-    currentPage: PropTypes.string,
-  }
-
+    currentPage: PropTypes.string
+  };
   state = {
     isSearching: false,
     searchValue: ''
@@ -22,30 +20,35 @@ class Iconography extends React.Component {
     document.title = `Carbon Design System | ${this.props.currentPage}`;
   }
 
-  handleSearch = (evt) => {
+  handleSearch = evt => {
     if (evt.target.value) {
       this.setState({
         isSearching: true,
-        searchValue: evt.target.value,
+        searchValue: evt.target.value
       });
     } else {
       this.setState({
         isSearching: false,
-        searchValue: '',
+        searchValue: ''
       });
     }
-  }
+  };
 
+  tempModifyIconName = name => {
+    return name.split('--').filter(string => string !== 'icon').join('--');
+  };
   renderIconCards = IconItems =>
-    Object.keys(IconItems).map((IconItem) => {
+    Object.keys(IconItems).map(IconItem => {
       const Icon = IconItems[IconItem];
-      const path = require(`../../assets/svg/${Icon.name}.svg`);
+      const path = require(`../../assets/svg/${this.tempModifyIconName(Icon.name)}.svg`);
       let iconElement;
-      if (!this.state.isSearching || Icon.name.includes(this.state.searchValue)) {
+      if (
+        !this.state.isSearching || Icon.name.includes(this.state.searchValue)
+      ) {
         iconElement = (
           <IconCard
             key={IconItem}
-            name={Icon.name}
+            name={this.tempModifyIconName(Icon.name)}
             viewBox={Icon.viewBox}
             height={Icon.height}
             width={Icon.width}
@@ -58,7 +61,7 @@ class Iconography extends React.Component {
 
   render() {
     const tabs = ['library', 'usage', 'contribution'];
-    const packageVer = Packages.dependencies['@console/bluemix-icons'];
+    const packageVer = Packages.dependencies['carbon-icons'];
     let currentPage = 'library';
     if (this.props.currentPage) {
       currentPage = this.props.currentPage;
@@ -71,16 +74,25 @@ class Iconography extends React.Component {
             <h2>Icon library</h2>
             <em className="version">Version {packageVer}</em>
             <div className="icon-container">
-              <Search small onChange={this.handleSearch} placeHolderText="Search the icon library" aria-label="Icon library search"></Search>
+              <Search
+                small
+                onChange={this.handleSearch}
+                placeHolderText="Search the icon library"
+                aria-label="Icon library search"
+              />
               {this.renderIconCards(IconList)}
             </div>
           </div>
         </Tab>
         <Tab href="/style/iconography/usage" label="Usage">
-          <MarkdownPage content={require('../../content/style/iconography/usage.md')} />
+          <MarkdownPage
+            content={require('../../content/style/iconography/usage.md')}
+          />
         </Tab>
         <Tab href="/style/iconography/contribution" label="Contribution">
-          <MarkdownPage content={require('../../content/style/iconography/contribution.md')} />
+          <MarkdownPage
+            content={require('../../content/style/iconography/contribution.md')}
+          />
         </Tab>
       </PageTabs>
     );
