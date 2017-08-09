f85f5bb992d00d6735772c9f032e4b86687f0982
MARI L. JOHANNESSEN <mjohannessen@us.ibm.com>
2017-08-09 13:18:16 -0500

    added simple tooltip to website (#1139)

--- src/content/components/tooltip/code.md
+++ src/content/components/tooltip/code.md
@@ -1,3 +1,8 @@
 ---
 desc: <span>Tooltips</span> provide additional information upon hover or focus. They often contain helper text that is contextual to an element.
+variations:
+  tooltip:
+  - Tooltip
+  tooltip--simple:
+  - Simple tooltip
 ---
--- src/data/components/tooltip.js
+++ src/data/components/tooltip.js
@@ -1,5 +1,13 @@
 /* eslint-disable */
  module.exports = {
   "desc": "<span>Tooltips</span> provide additional information upon hover or focus. They often contain helper text that is contextual to an element.",
+  "variations": {
+    "tooltip": [
+      "Tooltip"
+    ],
+    "tooltip--simple": [
+      "Simple tooltip"
+    ]
+  },
   "__content": "\n"
 }
