32a69e57109fdd9a014a9f30ca3e22ed5962fcbc
BRIAN T. HAN <bthan@us.ibm.com>
2017-08-09 19:41:45 -0500

    fix(loading): add small loading to code page (#1141)

--- src/data/components/loading.js
+++ src/data/components/loading.js
@@ -1,5 +1,13 @@
 /* eslint-disable */
  module.exports = {
   "desc": "<span>Loading</span> spinners are used when retrieving data or performing slow computations, and help to notify users that loading is underway.",
+  "variations": {
+    "loading": [
+      "Loading"
+    ],
+    "loading--small": [
+      "Small Loading"
+    ]
+  },
   "__content": "\n"
 }
