9dc184296ea0be98655e2620bae4d35feb7b61cb
TJ Egan <twegan@us.ibm.com>
2017-10-17 12:51:41 -0500

    feat(guidelines): start work on guidelines

--- src/app/components/custom/DosAndDonts/DosAndDonts1.js
+++ src/app/components/custom/DosAndDonts/DosAndDonts1.js
@@ -2,31 +2,27 @@ import React, { Component } from 'react';
 import Example from './Example.js';
 
 class DosAndDonts1 extends Component {
-
   render() {
     return (
       <div>
         <div className="do-and-dont">
           <Example
             correct
-            caption="Future Tense:"
+            caption="Future Tense"
             text="The API returns a promise."
           />
           <Example
-            caption="Future Tense:"
+            caption="Future Tense"
             text="The API will return a promise."
           />
         </div>
         <div className="do-and-dont">
           <Example
             correct
-            caption="Past tense:"
+            caption="Past tense"
             text="The limit was exceeded."
           />
-          <Example
-            caption="Past tense:"
-            text="The limit has been exceeded."
-          />
+          <Example caption="Past tense" text="The limit has been exceeded." />
         </div>
       </div>
     );
