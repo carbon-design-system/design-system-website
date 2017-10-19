9f18c8858695aeabbe18e67d3220866fb987a94f
Mari Johannessen <mari.l.johannessen@gmail.com>
2017-10-19 18:07:46 -0500

    Add pagination v2 (#63)
    * added pagination to page
    
    * finished up pagination

--- src/content/components/pagination/code.md
+++ src/content/components/pagination/code.md
@@ -1,3 +1,8 @@
 ---
 desc: <span>Pagination</span> is used for splitting up content or data into several pages, with a control for navigating to the next or previous page.
+variations:
+  pagination-v2:
+  - Pagination (v2)
+  pagination:
+  - Pagination (v1)
 ---
--- src/data/components/pagination.js
+++ src/data/components/pagination.js
@@ -1,5 +1,13 @@
 /* eslint-disable */
  module.exports = {
   "desc": "<span>Pagination</span> is used for splitting up content or data into several pages, with a control for navigating to the next or previous page.",
+  "variations": {
+    "pagination-v2": [
+      "Pagination (v2)"
+    ],
+    "pagination": [
+      "Pagination (v1)"
+    ]
+  },
   "__content": "\n"
 }
