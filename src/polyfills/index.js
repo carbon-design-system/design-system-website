6c04d5529925e71a3d08ee7a3b4bb188acfc0ae1
Akira Sudoh <asudoh@jp.ibm.com>
2017-11-15 11:46:08 +0900

    fix(polyfill): support IE11
    Refs carbon-design-system/carbon-components#433.

---
+++ src/polyfills/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import 'core-js/modules/es6.array.from';
+import 'core-js/modules/es6.math.sign';
+import 'core-js/modules/es6.object.assign';
+import 'core-js/modules/es6.promise.js';
+import 'core-js/modules/es6.string.includes';
+/* eslint-enable import/no-extraneous-dependencies */
+
+import './custom-event';
+import './element-closest';
+import './element-matches';
+import './toggle-class';
