2286ea590844122732cdd1600cff5d405dbe8775
mjohannessen <mjohannessen@us.ibm.com>
2018-02-02 15:04:12 -0600

    added small toggle component

--- src/content/components/toggle/code.md
+++ src/content/components/toggle/code.md
@@ -1,4 +1,9 @@
 ---
 desc: <span>Toggle</span> is a control that is used to quickly switch between two possible states.
+variations:
+  toggle:
+    - Toggle
+  toggle--small:
+    - Small toggle
 codepen: ooZqxw
 ---
--- src/data/components/toggle.js
+++ src/data/components/toggle.js
@@ -1,6 +1,14 @@
 /* eslint-disable */
  module.exports = {
   "desc": "<span>Toggle</span> is a control that is used to quickly switch between two possible states.",
+  "variations": {
+    "toggle": [
+      "Toggle"
+    ],
+    "toggle--small": [
+      "Small toggle"
+    ]
+  },
   "codepen": "ooZqxw",
   "__content": "\n"
 }
