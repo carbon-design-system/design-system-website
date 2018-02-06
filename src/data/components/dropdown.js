fb77e561b5cd47a55336d5a245afde6458b57f8a
Mari Johannessen <mari.l.johannessen@gmail.com>
2018-02-06 11:26:39 -0600

    added react dropdown components (#156)

--- src/data/components/dropdown.js
+++ src/data/components/dropdown.js
@@ -1,6 +1,20 @@
 /* eslint-disable */
  module.exports = {
   "desc": "<span>Dropdowns</span> present a list of options that can be used to filter existing content.",
+  "variations": {
+    "dropdown": [
+      "Dropdown"
+    ],
+    "MultiSelect": [
+      "Multi-select Dropdown"
+    ],
+    "MultiSelect.Inline": [
+      "Inline Multi-select Dropdown"
+    ],
+    "MultiSelect.Filterable": [
+      "Filter Dropdown"
+    ]
+  },
   "codepen": "eeGYvQ",
   "__content": "\n"
 }
