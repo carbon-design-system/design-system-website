ab16ff3dcd487b755c1ade8003f9a987dc324af6
Bethany <bethany.s@us.ibm.com>
2018-02-21 12:02:42 -0600

    added new PR stuff to FAQ

--- src/app/components/custom/ArticleTiles/ArticleTiles.js
+++ src/app/components/custom/ArticleTiles/ArticleTiles.js
@@ -5,16 +5,35 @@ class ArticleTiles extends Component {
   render() {
     const articleFour = require('../../../../content/getting-started/FAQ/images/article-4.png');
     const articleFive = require('../../../../content/getting-started/FAQ/images/article-5.png');
+    const articleSix = require('../../../../content/getting-started/FAQ/images/article-6.png');
 
     return (
       <div className="faq__tiles">
+      <ClickableTile
+        target="_blank"
+        className="faq__tile"
+        href="https://medium.com/carbondesign/data-table-updates-in-carbon-react-v5-22-0-6da0c24a96d6"
+      >
+        <div className="faq__tile-icon">
+          <img src={articleSix} alt="Data Table updates in Carbon React v5.22.0" />
+        </div>
+        <div className="faq__tile-info">
+          <h5>Data Table updates in Carbon React v5.22.0</h5>
+
+          <div className="faq__tile-caption">
+            <p>Josh Black</p>
+            <p>February 20, 2018</p>
+          </div>
+        </div>
+      </ClickableTile>
+
         <ClickableTile
           target="_blank"
           className="faq__tile"
           href="https://medium.com/design-ibm/introducing-carbon-themes-83d3985a8627"
         >
           <div className="faq__tile-icon">
-            <img src={articleFour} alt="Smashing Magazine article" />
+            <img src={articleFour} alt="Carbon themes" />
           </div>
           <div className="faq__tile-info">
             <h5>Introducing: Carbon Themes</h5>
@@ -32,7 +51,7 @@ class ArticleTiles extends Component {
           href="https://medium.com/design-ibm/carbon-designing-inside-big-blue-8577883cfe42"
         >
           <div className="faq__tile-icon">
-            <img src={articleFive} alt="Smashing Magazine article" />
+            <img src={articleFive} alt="Designing inside Big blue" />
           </div>
           <div className="faq__tile-info">
             <h5>Carbon: Designing inside Big Blue</h5>
