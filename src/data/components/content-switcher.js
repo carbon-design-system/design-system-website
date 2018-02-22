9798a999dfcd20fa7fe53f606048361c2f3a40b4
mjohannessen <mjohannessen@us.ibm.com>
2018-02-22 10:43:36 -0600

    deployment feb 22

--- src/data/components/content-switcher.js
+++ src/data/components/content-switcher.js
@@ -1,6 +1,21 @@
 /* eslint-disable */
  module.exports = {
   "desc": "<span>Content switcher</span> manipulates the content shown following an exclusive or “either/or” pattern.",
-  "codepen": "qVPBdz",
+  "variations": {
+    "content-switcher": [
+      "Content Switcher"
+    ],
+    "content-switcher--with-icon": [
+      "Content Switcher with icon"
+    ]
+  },
+  "codepen": {
+    "content-switcher": [
+      "qVPBdz"
+    ],
+    "content-switcher--with-icon": [
+      "qVPBdz"
+    ]
+  },
   "__content": "\n"
 }
