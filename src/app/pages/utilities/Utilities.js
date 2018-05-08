15bcc74242c2e478bb1ed656f3c0dda304441063
Tayler Aitken <tayler.aitken@gmail.com>
2018-05-08 12:25:49 -0500

    Utilities Section (#186)
    * addedin utility section
    
    * added in content and images for loading and overflow content
    
    * added in additional image
    
    * tinkering
    
    * disabled state update
    
    * content update
    
    * Added in filtering section to utilities, updated loading content, added a content fix to the data tables code page, edited/added content to the overflow content page
    
    * added in code documentation for skeleton states as well as images
    
    * updaded loading image and removed filtering from the interior left nav structure, was not ready to be pushed yet
    
    * loading content update
    
    * added documentation link for skeelton states
    
    * updated the code section on the loading content
    
    * added in svg for loading > skeleton state > code section
    
    * added in filtering section to interior left nav, upadated filtering content, added in filtering images

---
+++ src/app/pages/utilities/Utilities.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Page from '../../components/internal/Page';
+import MarkdownPage from '../../components/internal/MarkdownPage';
+
+class Utilities extends React.Component {
+  static propTypes = {
+    params: PropTypes.object,
+  }
+
+  render() {
+    const { params } = this.props;
+    const content = (
+      <MarkdownPage content={require(`../../../content/utilities/${params.name}/${params.name}.md`)} />
+    );
+    return (
+      <Page label="Utilities" title="" content={content} />
+    );
+  }
+}
+
+export default Utilities;
