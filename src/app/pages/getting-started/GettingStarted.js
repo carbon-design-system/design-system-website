d2aa95f71ee551f01a1dcf4722b1f325eeba4cd0
mjohannessen <mjohannessen@us.ibm.com>
2018-05-11 12:00:22 -0500

    added user flow section

--- src/app/pages/getting-started/GettingStarted.js
+++ src/app/pages/getting-started/GettingStarted.js
@@ -1,36 +1,41 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Page from '../../components/internal/Page';
-import FourOhFour from '../404';
+import React from "react";
+import PropTypes from "prop-types";
+import Page from "../../components/internal/Page";
+import FourOhFour from "../404";
 
-import MarkdownPage from '../../components/internal/MarkdownPage';
+import MarkdownPage from "../../components/internal/MarkdownPage";
 
-import ServiceProviders from './ServiceProviders';
+import ServiceProviders from "./ServiceProviders";
 
 class GettingStarted extends React.Component {
   static propTypes = {
-    params: PropTypes.object,
+    params: PropTypes.object
   };
 
   render() {
     const { params } = this.props;
-    const title = params.name.charAt(0).toUpperCase() + params.name.substring(1);
+    const title =
+      params.name.charAt(0).toUpperCase() + params.name.substring(1);
     const processENV = process.env.ENV;
-    if (params.name === 'service-providers' && !(processENV === 'internal')) {
+    if (params.name === "service-providers" && !(processENV === "internal")) {
       return <FourOhFour />;
     }
     let content;
-    if (params.name === 'service-providers') {
+    if (params.name === "service-providers") {
       content = <ServiceProviders currentPage={params.page} />;
     } else {
       content = (
-        <MarkdownPage content={require(`../../../content/getting-started/${params.name}/${params.name}.md`)} />
+        <MarkdownPage
+          content={require(`../../../content/getting-started/${params.name}/${
+            params.name
+          }.md`)}
+        />
       );
     }
     return (
       <Page
         label="Getting started"
-        title={title.replace(/-/g, ' ')}
+        title={title.replace(/-/g, " ")}
         content={content}
       />
     );
