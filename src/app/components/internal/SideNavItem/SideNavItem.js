99e80a2090e46fd9b20a0159b18fbf04febb4ad6
Akira Sudoh <asudoh@jp.ibm.com>
2018-05-12 20:13:33 +0900

    Fix JS error navigating to sub-expando with keyboard
    By letting React handle `tabIndex` property instead of doing so via DOM.
    
    Also:
    
    * Fixes the focus style of sub-expando
    * Supports opening/closing sub-expando with keyboard

--- src/app/components/internal/SideNavItem/SideNavItem.js
+++ src/app/components/internal/SideNavItem/SideNavItem.js
@@ -5,7 +5,7 @@ import classnames from 'classnames';
 class SideNavItem extends Component {
   static propTypes = {
     className: PropTypes.string,
-    children: PropTypes.node,
+    children: PropTypes.func,
     isCurrentPath: PropTypes.bool,
     isActiveItem: PropTypes.bool,
     type: PropTypes.string
@@ -46,57 +46,51 @@ class SideNavItem extends Component {
   };
 
   handleKeyDown = evt => {
-    if (evt.which === 13) {
-      const targetIsSubItem = evt.target.classList.contains('sub-nav__item') || evt.target.classList.contains('sub-nav__item-link');
-      const hasSubMenu = !(evt.currentTarget.querySelector('ul') === null);
-      if (!targetIsSubItem && hasSubMenu) {
-        const open = !this.state.open;
-        this.setState({ open });
-        const subMenu = evt.currentTarget.querySelector('ul');
-        if (!this.state.open) {
-          let height = 0;
-          [...subMenu.children].forEach(child => {
-            height += child.offsetHeight;
-          });
-          subMenu.style.maxHeight = `${height}px`;
-        } else {
-          const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
-          if (!evt.currentTarget.classList.contains('main-nav-item__open') && isIE11) {
-            subMenu.style.maxHeight = 0;
-          } else if (!isIE11) {
-            subMenu.style.maxHeight = 0;
-          }
+    if (evt.which === 13 && evt.target === evt.currentTarget) { // Does not handle bubbled up events
+      const open = !this.state.open;
+      this.setState({ open });
+      const subMenu = this.elem.querySelector('ul');
+      if (!this.state.open) {
+        let height = 0;
+        [...subMenu.children].forEach(child => {
+          height += child.offsetHeight;
+        });
+        subMenu.style.maxHeight = `${height}px`;
+      } else {
+        const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+        if (!this.elem.classList.contains('main-nav-item__open') && isIE11) {
+          subMenu.style.maxHeight = 0;
+        } else if (!isIE11) {
+          subMenu.style.maxHeight = 0;
         }
       }
     }
     if (evt.which === 39) {
       document.querySelector('#maincontent').focus();
     }
-    [...evt.currentTarget.querySelectorAll('.sub-nav__item')].forEach(link => {
-      const currentLink = link;
-      if (this.state.open) {
-        currentLink.querySelector('a').tabIndex = 0;
-      } else {
-        currentLink.querySelector('a').tabIndex = -1;
-      }
-    });
+  };
+
+  handleRef = elem => {
+    this.elem = elem;
   };
 
   render() {
+    const isSub = this.props.type === 'sub';
     const { children, isActiveItem } = this.props;
+    const { open } = this.state;
 
     const classNames = classnames({
       'main-nav-item': true,
-      'main-nav-item__open': this.state.open,
+      'main-nav-item__open': open,
       'main-nav-item__active': isActiveItem,
-      'main-nav-item--sub': this.props.type === 'sub'
+      'main-nav-item--sub': isSub,
     });
 
-    const tabIndex = children.length ? 0 : -1;
+    const tabIndex = children.length && !isSub ? 0 : undefined;
 
     return (
-      <li role="menuitem" tabIndex={tabIndex} className={classNames} onClick={this.handleClick} onKeyDown={this.handleKeyDown}>
-        {children}
+      <li role="menuitem" tabIndex={tabIndex} className={classNames} onClick={this.handleClick} onKeyDown={this.handleKeyDown} ref={this.handleRef}>
+        {children({ open, onKeyDown: this.handleKeyDown })}
       </li>
     );
   }
