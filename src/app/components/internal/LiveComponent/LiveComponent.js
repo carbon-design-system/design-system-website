99731b09bab70fc7e4bf4966c517c68c3f918805
mjohannessen <mjohannessen@us.ibm.com>
2018-05-30 16:18:41 -0500

    fixed urls

--- src/app/components/internal/LiveComponent/LiveComponent.js
+++ src/app/components/internal/LiveComponent/LiveComponent.js
@@ -11,15 +11,11 @@ class LiveComponent extends Component {
 
   componentDidMount() {
     let currentComponent =
-      this.props.component === 'detail-page-header--no-tabs' ||
-      this.props.component === 'detail-page-header--with-tabs'
+      this.props.component === 'detail-page-header--no-tabs' || this.props.component === 'detail-page-header--with-tabs'
         ? 'detail-page-header'
         : this.props.component;
-    currentComponent = currentComponent.replace(/-([a-z])/g, g =>
-      g[1].toUpperCase()
-    );
-    currentComponent =
-      currentComponent.charAt(0).toUpperCase() + currentComponent.substring(1);
+    currentComponent = currentComponent.replace(/-([a-z])/g, g => g[1].toUpperCase());
+    currentComponent = currentComponent.charAt(0).toUpperCase() + currentComponent.substring(1);
     if (currentComponent === 'Tabs') {
       currentComponent = 'Tab';
     } else if (currentComponent === 'Card') {
@@ -52,44 +48,23 @@ class LiveComponent extends Component {
     let htmlFile;
     if (this.props.component === 'text-input' && variation === 'text-area') {
       htmlFile = require('carbon-components/html/text-area/text-area.html');
-    } else if (
-      this.props.component === 'data-table' &&
-      variation === 'toolbar'
-    ) {
+    } else if (this.props.component === 'data-table' && variation === 'toolbar') {
       htmlFile = require('carbon-components/html/toolbar/toolbar.html');
     } else if (
       this.props.component === 'detail-page-header--no-tabs' ||
       this.props.component === 'detail-page-header--with-tabs'
     ) {
-      htmlFile = require(`carbon-components/html/detail-page-header/${
-        this.props.component
-      }.html`);
-    } else if (
-      this.props.component === 'data-table' &&
-      variation === 'data-table-v2'
-    ) {
+      htmlFile = require(`carbon-components/html/detail-page-header/${this.props.component}.html`);
+    } else if (this.props.component === 'data-table' && variation === 'data-table-v2') {
       htmlFile = require('carbon-components/html/data-table-v2/data-table-v2.html');
-    } else if (
-      this.props.component === 'data-table' &&
-      variation === 'data-table-v2--expandable'
-    ) {
+    } else if (this.props.component === 'data-table' && variation === 'data-table-v2--expandable') {
       htmlFile = require('carbon-components/html/data-table-v2/data-table-v2--expandable.html');
-    } else if (
-      this.props.component === 'data-table' &&
-      variation === 'data-table-v2--pagination'
-    ) {
+    } else if (this.props.component === 'data-table' && variation === 'data-table-v2--pagination') {
       htmlFile = require('carbon-components/html/data-table-v2/data-table-v2--pagination.html');
-    } else if (
-      this.props.component === 'data-table' &&
-      variation === 'data-table-v2--small'
-    ) {
+    } else if (this.props.component === 'data-table' && variation === 'data-table-v2--small') {
       htmlFile = require('carbon-components/html/data-table-v2/data-table-v2--small.html');
-    } else if (this.props.component === 'header') {
-      htmlFile = require('carbon-addons-bluemix/src/components/cloud-header/cloud-header.html');
     } else {
-      htmlFile = require(`carbon-components/html/${
-        this.props.component
-      }/${variation}.html`);
+      htmlFile = require(`carbon-components/html/${this.props.component}/${variation}.html`);
     }
 
     if (variation.includes('card')) {
@@ -99,15 +74,13 @@ class LiveComponent extends Component {
     }
 
     let headerContent =
-      this.props.component === 'detail-page-header--no-tabs' ||
-      this.props.component === 'detail-page-header--with-tabs' ? (
+      this.props.component === 'detail-page-header--no-tabs' || this.props.component === 'detail-page-header--with-tabs' ? (
         <div className="live-component-header" />
       ) : (
         ''
       );
     const variationLink =
-      this.props.component === 'detail-page-header--no-tabs' ||
-      this.props.component === 'detail-page-header--with-tabs'
+      this.props.component === 'detail-page-header--no-tabs' || this.props.component === 'detail-page-header--with-tabs'
         ? 'detail-page-header'
         : `${this.props.component}`;
     return (
@@ -116,14 +89,12 @@ class LiveComponent extends Component {
         <a
           href={`http://www.github.com/carbon-design-system/carbon-components/tree/master/src/components/${variationLink}`}
           target="_blank"
-          className="live-component__title">
+          className="live-component__title"
+        >
           {variation}.html
         </a>
         {headerContent}
-        <div
-          className={classNames}
-          dangerouslySetInnerHTML={{ __html: htmlFile }}
-        />
+        <div className={classNames} dangerouslySetInnerHTML={{ __html: htmlFile }} />
       </div>
     );
   };
@@ -142,20 +113,14 @@ class LiveComponent extends Component {
       component === 'order-summary'
     ) {
       backLink = `/add-ons/${component}/code`;
-    } else if (
-      component === 'detail-page-header--no-tabs' ||
-      component === 'detail-page-header--with-tabs'
-    ) {
+    } else if (component === 'detail-page-header--no-tabs' || component === 'detail-page-header--with-tabs') {
       backLink = '/add-ons/detail-page-header/code';
     } else {
       backLink = `/components/${component}/code`;
     }
 
     let content;
-    if (
-      component === 'detail-page-header--no-tabs' ||
-      component === 'detail-page-header--with-tabs'
-    ) {
+    if (component === 'detail-page-header--no-tabs' || component === 'detail-page-header--with-tabs') {
       variationContent = this.renderVariation(component);
       componentTitle = 'Detail page header';
     } else {
@@ -166,22 +131,14 @@ class LiveComponent extends Component {
       }
       const variations = content.variations;
       variationContent = content.variations
-        ? Object.keys(variations).map(variation =>
-            this.renderVariation(variation)
-          )
+        ? Object.keys(variations).map(variation => this.renderVariation(variation))
         : this.renderVariation(component);
-      componentTitle = `${component
-        .charAt(0)
-        .toUpperCase()}${component.substring(1)}`;
+      componentTitle = `${component.charAt(0).toUpperCase()}${component.substring(1)}`;
     }
     return (
       <div className="live-component">
         <Link to={backLink} className="bx--btn live-component__back-btn">
-          <Icon
-            className="live-component__back-btn--icon"
-            name="arrow--left"
-            description="Back button icon"
-          />
+          <Icon className="live-component__back-btn--icon" name="arrow--left" description="Back button icon" />
           {componentTitle} code page
         </Link>
         {variationContent}
--- src/content/getting-started/contributing/contributing.md
+++ src/content/getting-started/contributing/contributing.md
@@ -4,7 +4,9 @@ The guidelines below are primarily for **design contributions**. You can also re
 <br> <br>
 
 ## Contribution model & process
+
 ### 1. Submit an issue
+
 The easiest way for us to get an understanding of your needs is to open an issue. Follow our issue template (you'll see this when you go to open a new issue) to ensure you are meeting the issue writing requirements. Screenshots are required when submitting a new pattern or component. A member of the Carbon team will respond to your issue with next steps or a follow up.
 
 **Not sure where to open an issue?**
@@ -18,26 +20,24 @@ The [design-system-website repo](https://github.com/carbon-design-system/design-
 It's not so bad, we promise! You can read a [tutorial](https://help.github.com/articles/creating-an-issue/) on GitHub to get familiar with best practices.
 
 ### 2. Design review and sync
+
 The easiest way to move forward in the design process is to set up an in-person or remote call with the Carbon team to go over your design work. We'll provide you with feedback on your proposal and what, if any, changes are needed before bringing them into Carbon.
 
 _Please make sure you have run through these design guidelines prior to the initial design review meeting. The Carbon team will be looking for a few major things:_
-
 
 #### Accessible
 
 * Is your design [WCAG AA](https://www.w3.org/WAI/WCAG20/quickref/) accessible?
 * Does your design pass [color contrast](https://www.w3.org/TR/UNDERSTANDING-WCAG20/visual-audio-contrast-contrast.html) ratio?
-* Have you considered [keyboard tabbing](http://carbondesignsystem.com/guidelines/accessibility) and focus states?
-
+* Have you considered [keyboard tabbing](/guidelines/accessibility) and focus states?
 
 #### Componentized
+
 Most components we accept into the global library need to work universally across products.
 
 * Have you tested this design for multiple use cases? It needs to be a pattern that all teams can utilize, and global enough to fit use cases for other teams.
 * Is there a similar component in the library that already exists?
 * What are the advantages of this pattern over alternative patterns?
-
-
 
 #### Visual style and UX patterns
 
@@ -48,6 +48,6 @@ Most components we accept into the global library need to work universally acros
 
 <hr>
 
-
 ### 3. Roadmap
+
 We'll work with your team to determine when your design will be implemented into Carbon, and the logisitics behind it. Development resources are greatly appreciated and welcomed. If your pattern is submitted without any code, please keep in mind it may be delayed from getting released into the Carbon component library until we can fit it into our team sprint schedule.
