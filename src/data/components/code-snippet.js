68d2543395510ce02dcc20d444e9f93a3a1af9e9
mjohannessen <mjohannessen@us.ibm.com>
2018-05-30 15:20:19 -0500

    rebased the latest v9

--- src/data/components/code-snippet.js
+++ src/data/components/code-snippet.js
@@ -1,16 +1,28 @@
 /* eslint-disable */
-module.exports = {
-  desc: '<span>Code snippets</span> are small blocks of reusable code that can be inserted in a code file.',
-  variations: {
-    'code-snippet': ['Code Snippet'],
-    'code-snippet--inline': ['Inline Code Snippet'],
-    'code-snippet--multi': ['Multi Code Snippet'],
+ module.exports = {
+  "desc": "<span>Code snippets</span> are small blocks of reusable code that can be inserted in a code file.",
+  "variations": {
+    "code-snippet": [
+      "Code Snippet"
+    ],
+    "code-snippet--inline": [
+      "Inline Code Snippet"
+    ],
+    "code-snippet--multi": [
+      "Multi Code Snippet"
+    ]
   },
-  codepen: {
-    'code-snippet': ['YYqeGX'],
-    'code-snippet--inline': ['mprzKy'],
-    'code-snippet--multi': ['YYqeGX'],
+  "codepen": {
+    "code-snippet": [
+      "YYqeGX"
+    ],
+    "code-snippet--inline": [
+      "mprzKy"
+    ],
+    "code-snippet--multi": [
+      "YYqeGX"
+    ]
   },
-  __content: '\n',
-  metadata: {},
-};
+  "__content": "\n",
+  "metadata": {}
+}
