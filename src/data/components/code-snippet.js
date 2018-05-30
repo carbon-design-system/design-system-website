201209217925d8b3e7fdc9d3d00009148f26e1ad
mjohannessen <mjohannessen@us.ibm.com>
2018-05-30 15:05:59 -0500

    removed filtering

--- src/data/components/code-snippet.js
+++ src/data/components/code-snippet.js
@@ -7,12 +7,6 @@
     ],
     "code-snippet--terminal": [
       "Terminal"
-    ],
-    "code-snippet--inline": [
-      "Inline"
-    ],
-    "code-snippet--inline-light": [
-      "Inline (Light)"
     ]
   },
   "codepen": {
